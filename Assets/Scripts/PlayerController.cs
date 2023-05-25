4e708b811c68f7b99657dd437f96b007bed9c7da
Leamzi <ismael.serrada@gmail.com>
2023-05-25 12:53:08 -0300

    Add animation and sync animation amoung players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed;
     [SerializeField] PhotonView view;
+    [SerializeField] Animator animator;
+    [SerializeField] float speed;
     
+    static readonly int IsRunning = Animator.StringToHash("isRunning");
+
     void Update()
     {
         if (view.IsMine) Move();
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
         var moveAmount = moveInput.normalized * speed * Time.deltaTime;
         
         transform.position += (Vector3)moveAmount;
+        animator.SetBool(IsRunning, moveInput != Vector2.zero);
     }
 
 }
