b0a356ceb66acb0e241bf32549838bc12a53e362
Leamzi <ismael.serrada@gmail.com>
2023-05-25 10:14:57 -0300

    Join and create rooms

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Button createRoomButton, joinRoomButton;
+    [SerializeField] TMP_InputField createRoomInput;
+    [SerializeField] TMP_InputField joinRoomInput;
+    
+    const string GameScene = "GameScene";
+
+    void Start()
+    {
+        createRoomButton.onClick.AddListener(CreateRoom);
+        joinRoomButton.onClick.AddListener(JoinRoom);
+    }
+
+    void CreateRoom()
+    {
+        var roomOptions = new RoomOptions
+        {
+            MaxPlayers = 2
+        };
+           
+        PhotonNetwork.CreateRoom(createRoomInput.text, roomOptions);
+    }
+
+    void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinRoomInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(GameScene);
+    }
+}
