8016cd8dfa4cb8afbfaa520913792c6195904d38
Leamzi <ismael.serrada@gmail.com>
2023-05-25 11:56:50 -0300

    Add simple player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] PhotonView view;
+    
+    void Update()
+    {
+        if (view.IsMine) Move();
+    }
+
+    void Move()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        var verticalInput = Input.GetAxisRaw("Vertical");
+        var moveInput = new Vector2(horizontalInput, verticalInput);
+        var moveAmount = moveInput.normalized * speed * Time.deltaTime;
+        
+        transform.position += (Vector3)moveAmount;
+    }
+
+}
