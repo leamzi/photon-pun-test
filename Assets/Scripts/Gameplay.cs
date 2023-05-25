dde543e5433e14031dd3022da1094c5542eec836
Leamzi <ismael.serrada@gmail.com>
2023-05-25 11:45:45 -0300

    Spawn players with random position

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Gameplay : MonoBehaviour
+{
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] float spawnMinX, spawnMinY, spawnMaxX, spawnMaxY;
+
+    void Start()
+    {
+        var randomPosition = GetRandomPosition();
+        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+    }
+
+    Vector2 GetRandomPosition() => 
+        new(Random.Range(spawnMinX, spawnMaxX), Random.Range(spawnMinY, spawnMaxY));
+}
