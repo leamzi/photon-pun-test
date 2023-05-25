e22b1579e6472815d7ffecaaea31539e520fbae6
Leamzi <ismael.serrada@gmail.com>
2023-05-25 09:46:27 -0300

    Update packages: remove unused packages, add ui sample package. Create main menu scene. Load main menu scene after connection to master in photon is made succesfully

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,15 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    const string MainMenuScene = "MainMenu";
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log($"Connected to Master");
+        SceneManager.LoadScene(MainMenuScene);
+    }
+
+    void Start() => PhotonNetwork.ConnectUsingSettings();
+}
